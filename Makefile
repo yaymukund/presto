f9c195c9eb33c794793f36da4a43b1e2dbaf7db5
Mukund Lakshman <yaymukund@gmail.com>
2014-03-01 09:23:38 -0500

    v1.0 done!

--- Makefile
+++ Makefile
@@ -1,14 +1,19 @@
 .PHONY: all move_files build test
 
-all: move_files build test
+watcher = node_modules/nodemon/bin/nodemon.js
+
+all: build test
 
 move_files:
 	cp node_modules/mocha/mocha.js spec/vendor
 	cp node_modules/mocha/mocha.css spec/vendor
 	cp node_modules/expect.js/index.js spec/vendor/expect.js
 
-build:
+build: move_files
 	cat spec/*.js > spec/build/specs.js
+
+watch:
+	$(watcher) --exec make build --ignore spec/vendor/ --ignore spec/lib/
 
 test:
 	open spec/index.html
--- spec/presto_spec.js
+++ spec/presto_spec.js
@@ -1,14 +1,94 @@
 describe('Presto', function() {
+  $.mockjaxSettings.responseTime = 100;
+
   describe('.create', function() {
     it('returns a new presto instance', function() {
-      expect(Presto.create()).to.be.an('object');
+      expect(Presto.create({
+        bucketUrl: 'foo',
+        policyUrl: 'bar'
+      })).to.be.an('object');
+    });
+
+    it('requires a bucketUrl', function() {
+      expect(function() { Presto.create({policyUrl: 'foo'}); }).to.throwError();
+    });
+
+    it('requires a policyUrl', function() {
+      expect(function() { Presto.create({bucketUrl: 'foo'}); }).to.throwError();
     });
   });
 
   describe('.uploadFile', function() {
-    var presto = Presto.create();
+    var presto = Presto.create({
+      bucketUrl: 'http://test.host/bucketUrl',
+      policyUrl: 'http://test.host/policyUrl'
+    });
+
     it('returns a deferred promise', function() {
       expect(presto.uploadFile(null)).to.be.an('object');
     });
+
+    it('tries to hit policyUrl', function(done) {
+      var mid = mockGetPolicy({
+        response: function(req) {
+          $.mockjaxClear(mid);
+          expect(req.url).to.equal('http://test.host/policyUrl');
+          done();
+        }
+      });
+
+      presto.uploadFile(null);
+    });
+
+    it('tries to hit bucketUrl', function(done) {
+      mockGetPolicy();
+
+      var mid = mockPostBucket({
+        response: function(req) {
+          $.mockjaxClear(mid);
+          done();
+        }
+      });
+
+      presto.uploadFile('music');
+    });
+
+    it('sends formdata to the bucketUrl', function(done) {
+      mockGetPolicy();
+
+      var mid = mockPostBucket({
+        response: function(req) {
+          $.mockjaxClear(mid);
+          expect(req.data).to.be.a(FormData);
+          done();
+        }
+      });
+
+      presto.uploadFile('music');
+    });
   });
+
+  var mockGetPolicy = function(options) {
+    var mid, options = $.extend({
+      url: 'http://test.host/policyUrl',
+      type: 'GET',
+      responseText: {},
+      response: function(req) { $.mockjaxClear(mid); }
+    }, options || {});
+
+    mid = $.mockjax(options);
+    return mid;
+  };
+
+  var mockPostBucket = function(options) {
+    var mid, options = $.extend({
+      url: 'http://test.host/bucketUrl',
+      type: 'POST',
+      data: {},
+      response: function(req) { $.mockjaxClear(mid); }
+    }, options || {});
+
+    mid = $.mockjax(options);
+    return mid;
+  };
 });
