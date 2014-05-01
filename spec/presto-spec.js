d77b2a9f0c7db076abd79e50f99cd68cda7a8eee
Mukund Lakshman <yaymukund@gmail.com>
2014-04-30 21:10:32 -0400

    Don't completely give up if you can't get a cover image.

--- spec/presto-spec.js
+++ spec/presto-spec.js
@@ -12,4 +12,40 @@ describe('Presto', function() {
       done();
     });
   });
+
+  it('fails elegantly if the policy request fails', function(done) {
+    mockGetPolicy({ textStatus: 'getPolicy failure' });
+
+    Presto.upload(testFile).catch(function(error) {
+      expect(error.textStatus).to.be('getPolicy failure');
+      done();
+    });
+  });
+
+  it('fails elegantly if the S3 POST request fails', function(done) {
+    mockPostBucket({ textStatus: 'postBucket failure' });
+
+    Presto.upload(testFile).catch(function(error) {
+      expect(error.textStatus).to.be('postBucket failure');
+      done();
+    });
+  });
+
+  it('resolves with a null cover', function(done) {
+    mockFindTrack({ textStatus: 'findTrack failure' });
+
+    Presto.upload(testFile).then(function(response) {
+      expect(response.coverImageUrl).to.be.null;
+      done();
+    });
+  });
+
+  it('fails elegantly if the findCover request fails', function(done) {
+    mockFindCover({ textStatus: 'findCover failure' });
+
+    Presto.upload(testFile).then(function(response) {
+      expect(response.coverImageUrl).to.be.null;
+      done();
+    });
+  });
 });
